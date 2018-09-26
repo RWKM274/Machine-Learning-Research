d0c8556d6c0a22da3e0b276814106938d69bc7f1
Salem <SOzaki19@cornellcollege.edu>
2018-09-26 11:09:26 -0500

    add comment and organized

--- basicMachineLearning/sozakiBasicNerualNetwork.py
+++ basicMachineLearning/sozakiBasicNerualNetwork.py
@@ -1,25 +1,35 @@
 import numpy as np
+
+""" Takes in the number of neurons and the number of inputs
+	you would like and build a layer for you.
+	Each layer will have a input from the previous layer and
+	its neurons and its own neurons
+"""
 class NeuronLayer():
     def __init__(self, numberOfNeurons, numberOfInputsPerNeuron):
         self.synaptic_weights = 2 * np.random.random((numberOfInputsPerNeuron, numberOfNeurons)) - 1
 
 
+# Class that initializes with two layers that are the NeuronLayer class
 class NeuralNetwork():
 	def __init__(self, layer1, layer2):
 		self.layer1 = layer1
 		self.layer2 = layer2
 
 
-	# normalise input between 0 and 1
+	# takes in a number and normalizIndicates how confident we are about the existing weightes the number between 0 and 1
 	def __sigmoid(self, x):
 		return 1 / (1 + np.exp(-x))
 
 
-	# Indicates how confident we are about the existing weight
+	# Gives the amount of adjustment we need to give to the weights
 	def __sigmoid_derivative(self, x):
 		return x * (1 - x)
 
-	# We training the neural network
+	""" We are training the neural network, by taking in the training set,
+		then expected output set, and how many times we want the nerual
+		network to iterate through the training (epoches)
+	"""
 	def train(self, trainingSetInputs, trainingSetOutputs, numberOfTrainingIterations):
 		for iteration in range(numberOfTrainingIterations):
 
@@ -44,21 +54,23 @@ class NeuralNetwork():
 
 
 
-	# Thinking
+	# Using an input, it 'thinks' about what the outcome will be
 	def think(self, inputs):
 		outputFromLayer1 = self.__sigmoid(np.dot(inputs, self.layer1.synaptic_weights))
 		outputFromLayer2 = self.__sigmoid(np.dot(outputFromLayer1, self.layer2.synaptic_weights))
 		return outputFromLayer1, outputFromLayer2
 
-	# The neural network weights
+	# Prints out all of the neural network weights
 	def print_weights(self):
-		print ('		Layer 1 (4 neruons, each with 3 inputs): ')
+		print ('Layer 1 (4 neruons, each with 3 inputs): ')
 		print (self.layer1.synaptic_weights)
-		print (' 	Layer 2 (1 neruon, with 4 inputs):')
+		print ('Layer 2 (1 neruon, with 4 inputs):')
 		print (self.layer2.synaptic_weights)
 
-	# Relu
-	def relu(input):
+	""" steping: if input hits our threshold (0.5), then it fires an output,
+		otherwise it does not do anything
+	"""
+	def step(input):
 		if(input >= 0.5):
 			input = 1
 		else:
@@ -66,14 +78,12 @@ class NeuralNetwork():
 		return input
 
 
-""" the main function
-
-
+""" Main: creates a random seed and 
 """
 if __name__ == "__main__":
 
-	#Seed
-	np.random.seed(1)
+	# Seeding to enable us to go back and debug
+	np.random.seed(1054)
 
 	# Creating layer 1 (4 neurons, 3 inputs)
 	layer1 = NeuronLayer(4, 3)
@@ -84,6 +94,8 @@ if __name__ == "__main__":
 	# Create neral network
 	neuralNetwork = NeuralNetwork(layer1, layer2)
 
+	# prints all of the stages for easy read
+	# prints the first stage, which is the random weighting
 	print ('Stage 1: Randomizing synaptic weights: ')
 	neuralNetwork.print_weights()
 
@@ -94,10 +106,11 @@ if __name__ == "__main__":
 	# Train neural network with the training set, doing that 60,000 times
 	neuralNetwork.train(trainingSetIputs, trainingSetOutputs, 60000)
 
+	# Stage 2: Weights after training
 	print ('Stage 2: New synaptic weights after training: ')
 	neuralNetwork.print_weights()
 
-	# Test the new neural network
+	# Stage 3: Test the new neural network
 	print ('Stage 3: Consider a new situation [1, 1, 0] = ?')
 	hidden_state, output = neuralNetwork.think(np.array([1, 1, 0]))
 	reluOutput = neuralNetwork.relu(output)
