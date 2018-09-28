14160a9a708bced40ca344c89d10e229b42d18a5
pdemange <pdemange19@cornellcollege.edu>
2018-09-28 15:15:04 -0500

    Moved some files.

--- Keras_Practice/Convolutional_Neural_Network/pdemange/pdemange_cnn.py
+++ Keras_Practice/Convolutional_Neural_Network/pdemange/pdemange_cnn.py
@@ -2,6 +2,50 @@ from keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
 from keras import optimizers
+import os
+import numpy as np
+import pickle
+
+def outputOrganizer(x):
+	finder = {
+		'1':1,
+		'2':2,
+		'3':3,
+		'4':4,
+		'5':5,
+		'6':6,
+		'7':7,
+		'8':8,
+		'9':9,
+		'0':10,
+		'a':11,
+		'b':12,
+		'c':13,
+		'd':14,
+		'e':15,
+		'f':16,
+		'g':17,
+		'h':18,
+		'i':19,
+		'j':20,
+		'k':21,
+		'l':22,
+		'm':23,
+		'n':24,
+		'o':25,
+		'p':26,
+		'q':27,
+		'r':28,
+		's':29,
+		't':30,
+		'u':31,
+		'v':32,
+		'w':33,
+		'x':34,
+		'y':35,
+		'z':36
+	}
+	return finder.get(x)
 
 def createModel(): 
 	model = Sequential()
@@ -20,26 +64,43 @@ def createModel():
 	model.compile(loss='binary_crossentropy', optimizer=adamOpt, metrics=['accuracy'])
 	return model
 
+#Load the formatted data
+def loadData(xFile, yFile):
+	fX = open(xFile, 'r')
+	data_x = pickle.load(fX)
+	fX.close()
+
+	fY = open(yFile, 'r')
+	data_y = pickle.load(fY)
+	fY.close()
+
+	return data_x, data_y
+	
 #Do real quick formatting of the images for both testing and training!
-def formatData(): 
-	trainDataGen = ImageDataGenerator(
-		rescale=1./255,
-		shear_range=0.2,
-		zoom_range=0.2,
-		horizontal_flip=True)
+def formatData(cDirect, fName): 
+	data_x = []
+	data_y = []
+#	with open('training_x.txt', 'w+') as f:
+	counter = 0
+	for root, dirs, files in os.walk(cDirect): 
+		print(files[0])
+		for file in files:
+			name = file[:file.index('.')]
+			yName = np.array([])
+			immy = load_img(os.path.join(root,file))
+			immy = img_to_array(immy)
+			data_x.append(immy)
+			for char in name:
+				yName = np.append(yName, outputOrganizer(char))
+			data_y.append(yName)
+			counter += 1
+	print('Length of X data: '+str(len(data_x)))
+	print('Length of Y data: '+str(len(data_y)))
+	fx = open(fName+'_x.txt', 'w+')
+	pickle.dump(np.asarray(data_x), fx)
+	fx.close()
+	fy = open(fName+'_y.txt', 'w+')
+	pickle.dump(np.asarray(data_y), fy)
+	fy.close()
 
-	testDataGen = ImageDataGenerator(rescale=1./255)
 
-	trainGenerator = trainDataGen.flow_from_directory(
-		'./training',
-		target_size=(150,150),
-		batch_size=16, 
-		class_mode='binary')
-
-	testGenerator = testDataGen.flow_from_directory(
-		'./testing',
-		target_size=(150,150),
-		batch_size=16,
-		class_mode='binary')
-
-formatData()
