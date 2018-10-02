c4521d532f480b743c0defb577124b4daf84d81d
Salem Ozaki <SOzaki19@cornellcollege.edu>
2018-10-01 21:29:22 -0500

    modifed code for CNN

--- Keras_Practice/Convolutional_Neural_Network/sozaki/sozaki_fruit_recognition.py
+++ Keras_Practice/Convolutional_Neural_Network/sozaki/sozaki_fruit_recognition.py
@@ -3,7 +3,7 @@ from keras.models import Sequential, load_model
 from keras.preprocessing import image
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-from keras import backend as K
+from keras import backend as K, optimizers
 import os
 
 """ code based from a Keras Blog
@@ -25,7 +25,7 @@ test_dir = 'fruit_data/Test'
 
 train_sample = 2000
 test_sample = 800
-epochs = 20
+epochs = 4
 batch_size = 16
 classes = 3
 
@@ -36,10 +36,12 @@ input_shape = (img_width, img_height, 3)
 Debug = False
 
 def create_model():
+	optimization_rate = optimizers.rmsprop(lr=0.1)
 	neural_network = Sequential()
 	neural_network.add(Conv2D(32, (3, 3), input_shape=input_shape))
 	neural_network.add(Activation('relu'))
 	neural_network.add(MaxPooling2D(pool_size=(2, 2)))
+	neural_network.add(Dropout(0.5))
 
 	neural_network.add(Conv2D(32, (3, 3)))
 	neural_network.add(Activation('relu'))
@@ -48,6 +50,7 @@ def create_model():
 	neural_network.add(Conv2D(64, (3, 3)))
 	neural_network.add(Activation('relu'))
 	neural_network.add(MaxPooling2D(pool_size=(2, 2)))
+	neural_network.add(Dropout(0.5))
 
 	neural_network.add(Flatten())
 	neural_network.add(Dense(64))
@@ -76,7 +79,6 @@ def training_set():
 		target_size=(img_width, img_height),
 		batch_size=batch_size,
 		class_mode='categorical')
-
 	test_generator = test_augment.flow_from_directory(
 		test_dir,
 		target_size=(img_width, img_height),
@@ -120,17 +122,36 @@ def testing_neural_network(neural_network):
 		print(str(num_total_files) + ' total files')
 		print(str(num_correct) + ' correct ones')
 
-# # Creating and Traing our Convolutional Neural Network
 
-# # Creating the model
+# training our network and then testing our neural network
+def training_neural_network(neural_network, train_generator, test_generator):
+        neural_network.fit_generator(
+                train_generator,
+                steps_per_epoch=train_sample // batch_size,
+                epochs=epochs,
+#               validation_data=test_generator,
+#               validation_steps=test_sample // batch_size,
+                verbose=2)
+
+		
+# def evaluating():
+		
+
+
+
+# Creating and Traing our Convolutional Neural Network
+
+# Creating the model
 # neural_network = create_model()
 
-# # Returns the modification of the training set
-# train_generator, test_generator = training_set()
+# Returns the modification of the training set
+train_generator, test_generator = training_set()
+
+# Trains the Neural Network
+# training_neural_network(neural_network, train_generator, test_generator)
 
 
-
-# loading an existing Neural Network model
+# # loading an existing Neural Network model
 neural_network = load_model('neural_network.h5')
 
 
@@ -141,62 +162,51 @@ neural_network = load_model('neural_network.h5')
 testing_neural_network(neural_network)
 
 
-# # testing the neural network with customized images
-# list_of_options = ['apple', 'banana', 'pear']
-# num_total_files = 0
-# num_correct = 0
-# for root, dirs, files in os.walk('./test_data'):
-# 	for pics in files:
-# 		num_total_files += 1
-# 		file_name = 'test_data/' + pics
-# 		test_img = image.load_img(file_name, target_size=(img_width, img_height))
-# 		x = image.img_to_array(test_img)
-# 		inputx = x.reshape([-1, img_width, img_height, 3])
-# 		trailx = neural_network.predict(inputx, verbose=1)
-# 		print(pics)
-# 		# print(pics.split('_'))
-# 		# print(trailx)
+ # testing the neural network with customized images
+list_of_options = ['apple', 'banana', 'pear']
+num_total_files = 0
+num_correct = 0
+for root, dirs, files in os.walk('./test_data'):
+	for pics in files:
+		num_total_files += 1
+		file_name = 'test_data/' + pics
+		test_img = image.load_img(file_name, target_size=(img_width, img_height))
+		x = image.img_to_array(test_img)
+		inputx = x.reshape([-1, img_width, img_height, 3])
+		trailx = neural_network.predict(inputx, verbose=1)
+		print(pics)
+		# print(pics.split('_'))
+		# print(trailx)
+
 
 # 		""" this will patch the array of words and associates
-# 			it with the array returned from the CNN
-# 		"""
-# 		word_of_pic = pics.split('_')[0]
-# 		location_of_word_in_array = list_of_options.index(word_of_pic)
-# 		if(trailx[0][location_of_word_in_array] == 1):
-# 			num_correct += 1
-# 			print('correct')
-# 		else:
-# 			print('incorrect')
-# 		# if(pics.split('_')[0] in list_of_options):
-# 		# 	print('correct')
-# print(num_correct)
-# print(str(num_total_files) + ' total')
-# print('Accuacy: ' + str(num_correct/num_total_files * 100) + '%')
+#			it with the array returned from the CNN
+#		"""
+		word_of_pic = pics.split('_')[0]
+		location_of_word_in_array = list_of_options.index(word_of_pic)
+		if(trailx[0][location_of_word_in_array] == 1):
+			num_correct += 1
+			print('correct')
+		else:
+			print('incorrect')
+		if(pics.split('_')[0] in list_of_options):
+			print('correct')
+print(num_correct)
+print(str(num_total_files) + ' total')
+print('Accuacy: ' + str(num_correct/num_total_files * 100) + '%')
 
 
-
-
-# # training our network and then testing our neural network
-# neural_network.fit_generator(
-# 	train_generator,
-# 	steps_per_epoch=train_sample // batch_size,
-# 	epochs=epochs,
-# #	validation_data=test_generator,
-# #	validation_steps=test_sample // batch_size,
-# 	verbose=2)
-
-# # how to modify image
+# how to modify image
 # trial_augment = ImageDataGenerator(rescale=1. / 255)
-# # creating a manual test
+# creating a manual test
 # trial_gen = trial_augment.flow_from_directory(
-#                 'test_data',
-#                 target_size=(img_width, img_height),
-#                 batch_size=batch_size,
-#                 class_mode='categorical')
+     #   	'test_data',
+    #            target_size=(img_width, img_height),
+   #             batch_size=batch_size,
+  #              class_mode='categorical')
 
 # evaluating using my own dataset
 # trial_result = neural_network.predict_generator(trial_gen, verbose=1)
-
 
 
 
@@ -220,7 +230,8 @@ testing_neural_network(neural_network)
 # print(trial2)
 # print(type(trial_result))
 
+ending = neural_network.evaluate_generator(test_generator, verbose=1)
+print(ending)
 
-
-# # saving our trained neural network
+# saving our trained neural network
 # neural_network.save('neural_network.h5')
