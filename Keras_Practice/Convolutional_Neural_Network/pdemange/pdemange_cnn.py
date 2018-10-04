82bac149d30f5f92cc49b355955c9bde9ac81f3f
pdemange <pdemange19@cornellcollege.edu>
2018-10-03 21:22:41 -0500

    Final data cleaning.

--- Keras_Practice/Convolutional_Neural_Network/pdemange/pdemange_cnn.py
+++ Keras_Practice/Convolutional_Neural_Network/pdemange/pdemange_cnn.py
@@ -1,27 +1,29 @@
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img, load_img
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
+from keras.models import Model
+from keras.layers import Embedding,Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Input
 from keras.utils.np_utils import to_categorical
 from keras import optimizers
 import os
 import numpy as np
-import pickle
+import h5py
 
 
 def createModel(): 
-	model = Sequential()
 	#Create the Convolutional Model
-	model.add(Conv2D(32, (3,3), input_shape=(50,200,3), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2,2)))
-	model.add(Conv2D(32, (3,3), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2,2)))
-	model.add(Conv2D(64, (3,3), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2,2)))
+	inp = Input(shape=(50,200,3))
+	x = Conv2D(32, (3,3), input_shape=(50,200,3), activation='relu')(inp)
+	x = MaxPooling2D(pool_size=(2,2))(x)
+ 	x = Conv2D(32, (3,3), activation='relu')(x)
+	x = MaxPooling2D(pool_size=(2,2))(x)
+	x = Conv2D(64, (3,3), activation='relu')(x)
+	x = MaxPooling2D(pool_size=(2,2))(x)
 	#Flatten and connect it to a regular Dense Neural Network
-	model.add(Flatten())
-	model.add(Dense(64, activation='relu'))
-	model.add(Dense(35, activation='softmax'))
-	model.add(Dens)
+	x = Flatten()(x)
+	x = Dense(64, activation='relu')(x)
+	x = Dropout(0.5)(x)
+	preout1 = Dense(32, activation='relu')(x)
+	out1 = Dense(5*36, activation='softmax')(preout1)
+	model = Model(inputs=inp, outputs=out1)
 	adamOpt = optimizers.Adam(lr=.01)
 	model.compile(loss='binary_crossentropy', optimizer=adamOpt, metrics=['accuracy'])
 	return model
@@ -33,9 +35,10 @@ class dataHandler:
 		print(self.bList)
 
 	def genOutList(self):
-		tempy = [[0]]*36
+		tempy = np.array([[0]*36]*36)
+		print(tempy)
 		for x in range(0,36):
-			temp = [0]*36
+			temp = np.zeros(36)
 			temp[x] = 1
 			tempy[x] = temp
 		return tempy
@@ -46,14 +49,14 @@ class dataHandler:
 	def reverse(self, x):
 		return self.values[bList.index(x)]
 
-	def loadData(xFile, yFile):
-		fX = open(xFile, 'rb')
-		data_x = pickle.load(fX)
-		fX.close()
+	def loadData(self, xFile, yFile, alias):
+		fx = h5py.File(xFile, 'r')
+		data_x = fx[alias+'_x'][:]
+		fx.close()
 
-		fY = open(yFile, 'rb')
-		data_y = pickle.load(fY)
-		fY.close()
+		fy = h5py.File(yFile, 'r')
+		data_y = fy[alias+'_y'][:]
+		fy.close()
 
 		return data_x, data_y
 
@@ -64,37 +67,41 @@ class dataHandler:
 def formatData(cDirect, fName, dataOrg): 
 	data_x = []
 	data_y = []
-#	with open('training_x.txt', 'w+') as f:
 	counter = 0
 	for root, dirs, files in os.walk(cDirect): 
 		for file in files:
 			name = file[:file.index('.')]
-			yName = []
+			yName = np.array([])
 			immy = load_img(os.path.join(root,file))
 			immy = img_to_array(immy)
 			data_x.append(immy)
 			for char in name:
-				yName.append(dataOrg.lookup(char))
+				yName = np.append(yName, dataOrg.lookup(char))
 			data_y.append(yName)
 			counter += 1
-	print('Length of X data: '+str(len(data_x)))
-	print('Length of Y data: '+str(len(data_y)))
-	fx = open(fName+'_x.txt', 'wb+')
-	pickle.dump(np.asarray(data_x), fx)
-	fx.close()
-	fy = open(fName+'_y.txt', 'wb+')
-	pickle.dump(np.asarray(data_y), fy)
-	fy.close()
+	print(data_y)
+	#print('Length of X data: '+str(len(data_x)))
+	#print('Length of Y data: '+str(len(data_y)))
+	hf = h5py.File(fName+'_x.h5', 'w')
+	hf.create_dataset(fName+'_x', data=np.array(data_x))
+	hf.close()
+	hf = h5py.File(fName+'_y.h5', 'w')
+	hf.create_dataset(fName+'_y', data=np.array(data_y))
+	hf.close()
 
 def evaluateModel(network, xTest, yTest):
-	score = network.evaluate(xTest, np.asarray(yTest))
-	print('Testing Accuracy - %s : %.f2%%' % (network.metrics_names[1], score[1]*100))
+	score = network.evaluate(xTest, yTest)
+	print('Testing Accuracy - %s : %.2f%%' % (network.metrics_names[1], score[1]*100))
 
 if __name__ == '__main__': 
-	c = dataFinder()
+	c = dataHandler()
+	#print(c.lookup('1'))
 	formatData('train/training','training',c)
 	formatData('test/testing','testing',c)
-	f = open('training_y.txt','rb')
-	yTrain = pickle.load(f)
-	f.close()
-	print(yTrain)
+	xTrain, yTrain = c.loadData('training_x.h5', 'training_y.h5','training')
+	xTest, yTest = c.loadData('testing_x.h5', 'testing_y.h5','testing')
+	#print(type(yTrain))
+	#print(type(yTest))
+	net = createModel()
+	net.fit(xTrain, yTrain, epochs=20, batch_size=16)
+	evaluateModel(net, xTest, yTest)
