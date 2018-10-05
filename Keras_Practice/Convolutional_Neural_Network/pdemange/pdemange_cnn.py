04c8986a13c2ffb370e1398726bad1cb555830de
pdemange <pdemange19@cornellcollege.edu>
2018-10-04 23:41:06 -0500

    Starting from scratch.

--- Keras_Practice/Convolutional_Neural_Network/pdemange/pdemange_cnn.py
+++ Keras_Practice/Convolutional_Neural_Network/pdemange/pdemange_cnn.py
@@ -1,24 +1,25 @@
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img, load_img
-from keras.models import Model
+from keras.preprocessing.text import Tokenizer
+from keras.models import Model, model_from_json
 from keras.layers import Embedding,Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Input
 from keras.utils.np_utils import to_categorical
 from keras import optimizers
 import os
 import numpy as np
 import h5py
-
+from PIL import Image
+from time import sleep
 
 def createModel(): 
 	#Create the Convolutional Model
 	inp = Input(shape=(50,200,3))
 	x = Conv2D(32, (3,3), input_shape=(50,200,3), activation='relu')(inp)
 	x = MaxPooling2D(pool_size=(2,2))(x)
- 	x = Conv2D(32, (3,3), activation='relu')(x)
+	x = Conv2D(32, (3,3), activation='relu')(x)
 	x = MaxPooling2D(pool_size=(2,2))(x)
 	x = Conv2D(64, (3,3), activation='relu')(x)
 	x = MaxPooling2D(pool_size=(2,2))(x)
 	#Flatten and connect it to a regular Dense Neural Network
-	x = Flatten()(x)
 	x = Dense(64, activation='relu')(x)
 	x = Dropout(0.5)(x)
 	preout1 = Dense(32, activation='relu')(x)
@@ -32,11 +33,11 @@ class dataHandler:
 	def __init__(self):
 		self.values = '1234567890abcdefghijklmnopqrstuvwxyz'
 		self.bList = self.genOutList()
-		print(self.bList)
+		#print(self.bList)
 
 	def genOutList(self):
 		tempy = np.array([[0]*36]*36)
-		print(tempy)
+		#print(tempy)
 		for x in range(0,36):
 			temp = np.zeros(36)
 			temp[x] = 1
@@ -47,7 +48,39 @@ class dataHandler:
 		return self.bList[self.values.index(x)]
 
 	def reverse(self, x):
-		return self.values[bList.index(x)]
+		idx = np.argwhere(x==1)
+		print(idx)
+		return self.values[idx[0][0]]
+
+	def answer(self, resp): 
+		y = 0
+		ans = ''
+		for x in range(36, 226, 36):
+			if y == 180:
+				break
+			temp = resp[y:x]
+			print('Looking for: '+str(temp))
+			ans += self.reverse(temp)
+			y = x
+		return ans
+
+class DataHandler:
+	def __init__(self, allChars):
+		self.tok = self.createTokenizer(allChars)
+
+	def createTokenizer(self, chars):
+		temp = Tokenizer(char_level=True)
+		temp.fit_on_texts(chars)
+		return temp 
+
+	def toCategorical(self, text):
+		return to_categorical(self.tok.texts_to_sequences(text))
+
+	def fromCategorical(self, cat): 
+		temp = np.zeros(shape=(1,len(cat)))
+		for i in range(cat.shape[0]):
+			temp[0][i] = np.argmax(cat[i])
+		return self.tok.sequences_to_texts(temp)
 
 	def loadData(self, xFile, yFile, alias):
 		fx = h5py.File(xFile, 'r')
@@ -60,9 +93,38 @@ class dataHandler:
 
 		return data_x, data_y
 
+
+
 #Load the formatted data
 
+def loadNetwork(nFile):
+	with open(nFile+'_options.json', 'r') as f:
+		model = model_from_json(f.read())
+	model.load_weights(nFile+'_weights.h5')
+	adamOpt = optimizers.Adam(lr=.01)
+	model.compile(loss='binary_crossentropy', optimizer=adamOpt, metrics=['accuracy'])
+	return model
 	
+def saveNetwork(nNet, nFile):
+	with open(nFile+'_options.json', 'w') as f:
+		f.write(nNet.to_json())
+	nNet.save_weights(nFile+'_weights.h5')
+	print('Model saved!')
+
+def coolPredict(nNet, lookupHandler, vPredict, vAnswer=None):
+	val = Image.fromarray(vPredict[0], 'RGB')
+	val.show()
+	sleep(5)
+	ans = nNet.predict(vPredict)
+	tAns = lookupHandler.answer(ans[0])
+	if vAnswer is not None:
+		temp = lookupHandler.answer(vAnswer)
+		if tAns == temp:
+			print(tAns+' == '+temp)
+		else:
+			print(tAns+' != '+temp)
+	input('Press enter to continue...')
+
 #Do real quick formatting of the images for both testing and training!
 def formatData(cDirect, fName, dataOrg): 
 	data_x = []
@@ -94,14 +156,23 @@ def evaluateModel(network, xTest, yTest):
 	print('Testing Accuracy - %s : %.2f%%' % (network.metrics_names[1], score[1]*100))
 
 if __name__ == '__main__': 
-	c = dataHandler()
+	c = DataHandler('1234567890abcdefghijklmnopqrstuvwxyz')
+	tt = c.toCategorical('1234567890abcdefghijklmnopqrstuvwxyz')
+	print(tt)
+	print(c.fromCategorical(tt))
 	#print(c.lookup('1'))
-	formatData('train/training','training',c)
-	formatData('test/testing','testing',c)
-	xTrain, yTrain = c.loadData('training_x.h5', 'training_y.h5','training')
-	xTest, yTest = c.loadData('testing_x.h5', 'testing_y.h5','testing')
+	#formatData('train/training','training',c)
+	#formatData('test/testing','testing',c)
+	#xTrain, yTrain = c.loadData('training_x.h5', 'training_y.h5','training')
+	#xTest, yTest = c.loadData('testing_x.h5', 'testing_y.h5','testing')
+	#net = loadNetwork('captcha')
 	#print(type(yTrain))
 	#print(type(yTest))
-	net = createModel()
-	net.fit(xTrain, yTrain, epochs=20, batch_size=16)
-	evaluateModel(net, xTest, yTest)
+	#net = createModel()
+	#for x_data, y_data in zip(xTrain, yTrain):
+	#net.fit(np.array([x_data]), np.array([y_data]), epochs=20)
+	#evaluateModel(net, xTest, yTest)
+	#print(net.predict(np.array([xTest[0]])))
+	#for inc in range(len(xTest)):
+#		coolPredict(net, c, np.array([xTest[inc]]), yTest[inc])
+	#saveNetwork(net,'captcha')
