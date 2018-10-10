f0fc4aa6b120f4110bd152a7b770573d230ea26e
Salem Ozaki <SOzaki19@cornellcollege.edu>
2018-10-10 09:27:07 -0500

    got caption RNN to work

--- Keras_Practice/Recurrent_Neural_Network/sozaki/caption_recurrent_neural_network.py
+++ Keras_Practice/Recurrent_Neural_Network/sozaki/caption_recurrent_neural_network.py
@@ -1,8 +1,18 @@
 import youtube_dl
 import re
 import webvtt
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.callbacks import LambdaCallback
+from keras.models import Sequential, load_model
 
 max_len_of_sent = 40
+
+# variables for testing the model every so often
+epochs_until_test = 10
+length_of_text_to_generate = 100
 
 # credit: pdemange. From his collector.py
 class CaptionCollector:
@@ -55,17 +65,87 @@ class CaptionCollector:
 class directory_of_letters:
 
     def __init__(self, list):
-        self.dictionary = dict()
+        self.int_to_char = dict()
+        self.char_to_int = dict()
         for i in range(len(list)):
-            self.dictionary[i] = list[i]
+            self.int_to_char[i] = list[i]
+            self.char_to_int[list[i]] = i
+
+def prepare_data(list, num_unique_letters, dic_of_letters, steps=3):
+    x_raw = []
+    y_raw = []
+    for i in range(0, len(list) - max_len_of_sent, steps):
+        x_raw.append(list[i:i + max_len_of_sent])
+        y_raw.append(list[i + max_len_of_sent])
+    x_train = np.zeros((len(y_raw), max_len_of_sent, num_unique_letters), dtype=np.bool)
+    y_train = np.zeros((len(y_raw), num_unique_letters), dtype=np.bool)
+
+    # converting letters into array of 0 and 1 for the appropriate letter
+    for t, sentence in enumerate(x_raw):
+        for loc, letters in enumerate(sentence):
+            # print(t, loc, letters, dic_of_letters.char_to_int[letters])
+            x_train[t][loc][dic_of_letters.char_to_int[letters]] = 1
+
+    for t, sentence in enumerate(y_raw):
+        for loc, letters in enumerate(sentence):
+            # print(t, loc, letters, dict_of_letters.char_to_int[letters])
+            y_train[t][dic_of_letters.char_to_int[letters]] = 1
+    return x_train, y_train
+
+def create_model(num_unique_letters):
+    model = Sequential()
+    model.add(LSTM(128, return_sequences=True, input_shape=(max_len_of_sent, num_unique_letters)))
+    model.add(LSTM(64))
+    model.add(Dense(num_unique_letters, activation='softmax'))
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.optimizer.lr = 0.01
+    return model
+
+def convert_text_to_array(raw_text, num_unique_letters, dic_of_letters):
+    zero_array = np.zeros((len(raw_text), max_len_of_sent, num_unique_letters), dtype=np.bool)
+
+    # converting letters into array of 0 and 1 for the appropriate letter
+    for t, sentence in enumerate(raw_text):
+        for loc, letters in enumerate(sentence):
+            # print(t, loc, letters, dic_of_letters.char_to_int[letters])
+            zero_array[t][loc][dic_of_letters.char_to_int[letters]] = 1
+    return zero_array
+
+def on_epoch_end(epoch, logs):
+    if(epoch % epochs_until_test == 0):
+        full_generated_text = ""
+        print('Testing time!')
+        test_text = "but anyway you guys are gonna see how th"
+        full_generated_text = full_generated_text + test_text
+
+        for i in range(length_of_text_to_generate):
+            test_array = convert_text_to_array(test_text, number_of_unique_letters, dict_of_letters)
+            array_answer = model.predict(test_array, verbose=0)[0]
+            added_letter = dict_of_letters.int_to_char[np.argmax(array_answer)]
+            # print(added_letter)
+            test_text = test_text + added_letter
+            test_text = test_text[1:]
+            full_generated_text = full_generated_text + added_letter
+            print(full_generated_text)
+
+
 
 if __name__ == '__main__':
     captions_class = CaptionCollector()
+
     # captions_class.downloadSubs("https://youtu.be/otwkRq_KnG0", "8-bitryan")
-    caption = captions_class.readAllCaptions('8-bitryan.en.vtt')
-    sent = str.join(' ', caption)
-    ordered_list = sorted(list(set(sent.lower())))
-    dict_of_letters = directory_of_letters(ordered_list)
-    print(dict_of_letters.dictionary)
-    print(dict_of_letters.dictionary[1])
-    # print(sent)
+
+
+    # caption = captions_class.readAllCaptions('8-bitryan.en.vtt')
+    # sent = str.join(' ', caption).lower()
+    # ordered_list = sorted(list(set(sent)))
+    # dict_of_letters = directory_of_letters(ordered_list)
+    # number_of_unique_letters = len(set(sent.lower()))
+    #
+    # # pass in the sentence and the number of unique letters
+    # x, y = prepare_data(sent, number_of_unique_letters, dict_of_letters)
+    # # model = load_model('caption.h5')
+    # model = create_model(number_of_unique_letters)
+    # model.fit(x, y, batch_size=256, epochs=1000, verbose=1, callbacks=[LambdaCallback(on_epoch_end=on_epoch_end)])
+    # model.save('caption.h5')
+    #
